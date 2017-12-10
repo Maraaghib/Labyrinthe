61287ed68a4fea3d4e940cdba8dba062190814d5
Serigne Amsatou SEYE <serigne-amsatou.seye@etu.u-bordeaux.fr>
2017-12-10 19:43:47 +0100

    Rajout des fichiers supprimés

---
+++ src/model/Edge.java
@@ -0,0 +1,124 @@
+/**
+ * 
+ */
+package model;
+
+import org.jgrapht.graph.DefaultEdge;
+
+/**
+ * @author hamza
+ *
+ */
+public class Edge implements Comparable<Edge> {
+	
+	private Vertex source;
+	private Vertex target;
+
+	public enum Type{
+		OPENED_DOOR,
+		CLOSED_DOOR,
+		CORRIDOR;
+	};
+	
+	private Type type;
+	
+	public Edge(Type type) {
+//		super();
+		this.type = type;
+	}
+	
+	public Edge(Vertex source, Vertex target) {
+//		super();
+		this.source = source;
+		this.target = target;
+	}
+
+	public Edge(Vertex source, Vertex target, Type type) {
+		this.source = source;
+		this.target = target;
+		this.type = type;
+	}
+
+	// default
+	public Edge() {
+		super();
+		this.type = Type.CORRIDOR;
+	}
+	
+	public void setSource(Vertex source) {
+		this.source = source;
+	}
+
+	public void setTarget(Vertex target) {
+		this.target = target;
+	}
+
+	public Vertex getSource() {
+		Vertex vertex = null;
+		try {
+			 vertex = (Vertex) this.source.clone();
+		} catch (CloneNotSupportedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return vertex;
+	}
+	
+	public Vertex getTarget() {
+		Vertex vertex = null;
+		try {
+			vertex = (Vertex) this.target.clone();
+		} catch (CloneNotSupportedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return vertex;
+	}
+
+	public Type getType() {
+		return type;
+	}
+
+	public void setType(Type type) {
+		this.type = type;
+	}
+	
+
+//	@Override
+//	public boolean equals(Object obj) {
+//		// TODO Auto-generated method stub
+//		return this.getSource().equals(((Edge)obj).getSource()) && this.getTarget().equals(((Edge)obj).getTarget());
+//	}
+
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		// TODO Auto-generated method stub
+		return new Edge((Vertex)this.getSource().clone(), (Vertex)this.getTarget().clone(), this.getType());
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return (this.getSource().equals(((Edge)obj).getSource()) && this.getTarget().equals(((Edge)obj).getTarget())) || (this.getSource().equals(((Edge)obj).getTarget()) && this.getTarget().equals(((Edge)obj).getSource()));
+	}
+
+	@Override
+	public String toString() {
+		return "Edge [" + source + "---" + target + "]";
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Comparable#compareTo(java.lang.Object)
+	 */
+	@Override
+	public int compareTo(Edge o) {
+		// TODO Auto-generated method stub
+		int source = this.getSource().compareTo((o).getSource());
+		if (source != 0) {
+			return source;
+		} else {
+			return this.getTarget().compareTo((o).getTarget());
+		}
+	}
+
+}
---
+++ src/model/Vertex.java
@@ -0,0 +1,112 @@
+package model;
+
+import model.Labyrinth.Directions;
+
+public class Vertex implements Comparable<Vertex> {
+
+	private int nbr;
+	private int x;
+	private int y;
+
+	public Vertex() {
+//		super();
+		this.setX(0);
+		this.setY(0);
+		this.setNbr(0);
+	}
+
+	public Vertex(int x, int y) {
+//		super();
+		this.setX(x);
+		this.setY(y);
+	}
+
+	public Vertex(int x, int y, int nbr) {
+//		super();
+		this.setX(x);
+		this.setY(y);
+		this.setNbr(nbr);
+	}
+
+	public int getNbr() {
+		return this.nbr;
+	}
+
+	public void setNbr(int nbr) {
+		//if(nbr <= (Labyrinth.WIDTH * Labyrinth.HEIGHT)) {
+			this.nbr = nbr;
+		//}
+	}
+
+	public int getX() {
+		return this.x;
+	}
+
+	public void setX(int x) {
+		if (x <= Labyrinth.WIDTH) {
+			this.x = x;
+		}
+	}
+
+	public int getY() {
+		return this.y;
+	}
+
+	public void setY(int y) {
+		if (y <= Labyrinth.HEIGHT) {
+			this.y = y;
+		}
+	}
+
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		// TODO Auto-generated method stub
+		Vertex vertex = new Vertex(this.getX(), this.getY(), this.getNbr());
+		return vertex;
+	}
+
+	public int compareTo(Vertex source) {
+		// TODO Auto-generated method stub
+		if(this.x != source.x) {
+			return source.x - this.x;
+		}
+		else {
+			return source.x - this.y;
+		}
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return this.getX() == ((Vertex)obj).getX() && this.getY() == ((Vertex)obj).getY();
+	}
+
+	@Override
+	public String toString() {
+		return "("+this.getX()+" , "+this.getY()+")";
+//		return "Vertex [nbr=" + this.getNbr()+ ", x=" + this.getX() + ", y=" + this.getY() + "]";
+	}
+
+	public boolean inBorders(Directions dir) {
+		// TODO Auto-generated method stub
+		Vertex vertex = new Vertex(this.getX(), this.getY());
+		boolean inBorder = false;
+		switch (dir) {
+		case NORTH:
+			inBorder = ((vertex.getY()-1) >= Labyrinth.TOP_BORDER) ? true : false;
+			break;
+		case SOUTH:
+			inBorder = ((vertex.getY()+1) <= Labyrinth.DOWN_BORDER) ? true : false;
+			break;
+		case EAST:
+			inBorder = ((vertex.getX()+1) <= Labyrinth.RIGHT_BORDER) ? true : false;
+			break;
+		case WEST:
+			inBorder = ((vertex.getX()-1) >= Labyrinth.LEFT_BORDER) ? true : false;
+			break;
+	}
+		return inBorder;
+	}
+
+
+}
